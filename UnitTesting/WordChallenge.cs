328ab080066c0fd02b8742d169d17781cf353e51
AnneScheyltjens <annescheyltjens@hotmail.com>
2024-06-07 17:41:44 +0200

    Rework and add tests for the new code

--- UnitTesting/WordChallenge.cs
+++ UnitTesting/WordChallenge.cs
@@ -9,14 +9,15 @@ public class WordChallenge
     public void Setup()
     {
         Program._fullSizedWords = new List<string>();
-        Program._notFullSizedWords = new List<Word>();
+        Program._notFullSizedWords = new List<string>();
+        Program._CombinationWords = new List<CombinationWord>();
     }
 
     [DataTestMethod]
     [DataRow("foo", "bar", "foobar", 1, 2)]
     [DataRow("giblet", "tromie", "leader", 3, 0)]
     [DataRow("g", "tr", "lead", 0, 3)]
-    public void SplitWordList(string word1, string word2, string word3, int fullSize, int notFullSize)
+    public void SplitWordListTest(string word1, string word2, string word3, int fullSize, int notFullSize)
     {
         //Arrange
         var words = new List<string>()
@@ -33,25 +34,71 @@ public class WordChallenge
     }
 
     [TestMethod]
-    public void MakeCombinations()
+    public void MakeCombinationsTest()
     {
         //Arrange
-        var wordToCheck = new Word() { Value = "bar" };
         const string fullSize = "foobar";
-        
+
         Program._fullSizedWords.Add(fullSize);
-        Program._notFullSizedWords.AddRange(new List<Word>()
+        Program._notFullSizedWords.AddRange(new List<string>()
         {
-            new Word() {Value = "foo"},
-            wordToCheck
+            "foo",
+            "bar"
         });
-        
+
         //Act
-        Program.MakeCombinations(wordToCheck);
-        
+        Program.MakeCombinations();
+
         //Assert
-        Assert.AreEqual(1 ,Program._notFullSizedWords.Count(word => word.Value == wordToCheck.Value));
-        var combinations = Program._notFullSizedWords.Single(word => word.Value == wordToCheck.Value).Combinations;
-        Assert.AreEqual(fullSize, combinations.First().Value);
+        Assert.AreEqual(1, Program._CombinationWords.Count);
+        Assert.AreEqual(fullSize, Program._CombinationWords[0].Value);
     }
+    
+    [TestMethod]
+    public void FilterCriteriaTest()
+    {
+        //Arrange
+        var overMaxLength = new CombinationWord() {Value = "foobary", WordsNeeded = new List<string>() {"foobar", "y"}};
+        
+        Program._fullSizedWords.Add("foobar");
+        Program._CombinationWords = new List<CombinationWord>()
+        {
+            new CombinationWord() {Value = "foo", WordsNeeded = new List<string>() {"fo", "o"}},
+            new CombinationWord() {Value = "bar", WordsNeeded = new List<string>() {"b", "ar"}},
+            new CombinationWord() {Value = "lepel", WordsNeeded = new List<string>() {"lep", "el"}},
+            overMaxLength
+        };
+
+        //Act
+        Program.FilterOnCriteria();
+
+        //Assert
+        Assert.AreEqual(2, Program._CombinationWords.Count);
+        Assert.IsFalse(Program._CombinationWords.Contains(overMaxLength));
+    }
+    
+     [TestMethod]
+    public void AddAnotherWordToCombosTest()
+    {
+        //Arrange
+        Program._notFullSizedWords = new List<string>()
+        {
+            "fo", "o", "b", "ar"
+        };
+        
+        Program._CombinationWords = new List<CombinationWord>()
+        {
+            new CombinationWord() {Value = "foo", WordsNeeded = new List<string>() {"fo", "o"}},
+            new CombinationWord() {Value = "bar", WordsNeeded = new List<string>() {"b", "ar"}},
+        };
+
+        //Act
+        Program.AddAnotherWordToCombos();
+
+        //Assert
+        Assert.AreEqual(4, Program._CombinationWords.Count);
+        Assert.AreEqual("foob", Program._CombinationWords[0].Value);
+    }
+    
+    
 }
