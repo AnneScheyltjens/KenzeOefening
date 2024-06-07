8b261909a373e6731ac5d454c4188da3ce3d92d9
AnneScheyltjens <annescheyltjens@hotmail.com>
2024-06-07 17:39:29 +0200

    Add method to easily add new word and make constructor to copy the element to new object

--- Kenze6LetterWordChallenge/CombinationWord.cs
+++ Kenze6LetterWordChallenge/CombinationWord.cs
@@ -5,6 +5,25 @@ public class CombinationWord
     public List<string> WordsNeeded { get; set; }
     public string Value { get; set; }
 
+    public CombinationWord()
+    {
+        Value = "";
+        WordsNeeded = new List<string>();
+    }
+
+    public CombinationWord(CombinationWord combo)
+    {
+        Value = combo.Value;
+        WordsNeeded = new List<string>();
+        combo.WordsNeeded.ForEach(word => WordsNeeded.Add(word));
+    }
+
+    public void AddWord(string word)
+    {
+        Value += word;
+        WordsNeeded.Add(word);
+    }
+
     public override string ToString()
     {
         var result = "";
